71b963b864b3536b23bd7243114ece0eaa00fd68
Sumiran <43204798+antihero008@users.noreply.github.com>
2020-10-15 19:48:56 +0530

    First Commit

---
+++ src/app/predef-todo.ts
@@ -0,0 +1,7 @@
+import {Todo} from './todo';
+export const Todos: Todo[] = [
+{ id : 1, date : '15/10/2020', title : 'Coding', description : 'To Code' },
+{ id : 2, date : '16/10/2020', title : 'Assignment', description : 'Complete the Assignment'},
+{ id : 3, date : '17/10/2020', title : 'Shopping', description : 'Shop for Groceries'},
+{ id : 4, date : '18/10/2020', title : 'Laundry', description : 'Do the Laundry'}
+];
